c6d92091303c1bed1b9a885891e97d9ce4940da5
Gerardo-pro <84603900+Gerardo-pro@users.noreply.github.com>
2021-12-02 19:01:45 -0600

    app rest
    continuación de uso de servicios REST unicamente HTML simple sin CSS

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-#AQUI VAMOS APEGAR EL CODIGO
+#EQUIPO 2
+# LUIS FERNANDO ROJAS
+# LEONARDO RUIZ RODRIGUEZ
+# GERARDO CASTAÑEDA
 
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -15,6 +18,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #INSTANCIAR CONEXION
 db=SQLAlchemy(app)
 ma= Marshmallow(app)
+ema = Marshmallow(app)
+
 
 #CREACION DE LA TABLA Y LA CLASE
 class Categoria(db.Model):
@@ -28,30 +33,30 @@ class Categoria(db.Model):
 
 
 class Empleados(db.Model):
-    emp_id_nomina = db.Column(db.Integer, primary_key=True)
-    emp_nombre = db.Column(db.String(50))
-    emp_domicilio = db.Column(db.String(50))
-    emp_telefono = db.Column(db.String(15))
-    emp_rfc = db.Column(db.String(13))
-    emp_curp = db.Column(db.String(18))
-    emp_imss = db.Column(db.String(11))
-    emp_id_puesto = db.Column(db.Integer)
-    emp_id_jornada = db.Column(db.Integer)
-    emp_edoEmpleado = db.Column(db.String(1))
-    emp_fechaingreso = db.Column(db.String(10))
-    emp_ctaBancaria = db.Column(db.String(18))
+    id_nomina = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50))
+    domicilio = db.Column(db.String(50))
+    telefono = db.Column(db.String(15))
+    rfc = db.Column(db.String(13))
+    curp = db.Column(db.String(18))
+    imss = db.Column(db.String(11))
+    id_puesto = db.Column(db.Integer)
+    id_jornada = db.Column(db.Integer)
+    edoEmpleado = db.Column(db.String(1))
+    fechaingreso = db.Column(db.String(10))
+    ctaBancaria = db.Column(db.String(18))
     def __init__(self, nombre, domicilio, telefono, rfc, curp, imss, id_puesto, id_jornada, edoEmpleado, fechaingreso, ctaBancaria):
-        self.emp_nombre = nombre
-        self.emp_domicilio = domicilio
-        self.emp_telefono = telefono
-        self.emp_rfc= rfc
-        self.emp_curp = curp
-        self.emp_imss = imss
-        self.emp_id_puesto = id_puesto
-        self.emp_id_jornada = id_jornada
-        self.emp_edoEmpleado = edoEmpleado
-        self.emp_fechaingreso = fechaingreso
-        self.emp_ctaBancaria = ctaBancaria
+        self.nombre = nombre
+        self.domicilio = domicilio
+        self.telefono = telefono
+        self.rfc= rfc
+        self.curp = curp
+        self.imss = imss
+        self.id_puesto = id_puesto
+        self.id_jornada = id_jornada
+        self.edoEmpleado = edoEmpleado
+        self.fechaingreso = fechaingreso
+        self.ctaBancaria = ctaBancaria
 
 db.create_all()
 
@@ -64,10 +69,9 @@ categoria_schema = CategoriaSchema()
 #cuando sean muchas respuestas
 categorias_schema = CategoriaSchema(many=True)
 
-class EmpleadoSchema(ma.Schema):
+class EmpleadoSchema(ema.Schema):
     class Meta:
-        fields = ('nopmbre', 'direccion', 'telefono','rfc', 'curp','imss', 'id_puesto','id_jornada','edoEmpleado',
-                  'fechaingreso','ctaBancaria')
+        fields = ('nombre', 'domicilio', 'telefono','rfc', 'curp','imss', 'id_puesto','id_jornada','edoEmpleado','fechaingreso','ctaBancaria')
 
 empleado_schema = EmpleadoSchema()
 empleados_schema = EmpleadoSchema(many=True)
@@ -79,11 +83,11 @@ def get_categorias():
     result = categorias_schema.dump(all_categorias)
     return jsonify(result)
 
-@app.route('/empleados', methods=['GET'])
+@app.route('/empleado', methods=['GET'])
 def get_empleados():
     all_empleados = Empleados.query.all()
-    resultado = empleados_schema.dump(all_empleados)
-    return jsonify(resultado)
+    result = empleados_schema.dump(all_empleados)
+    return jsonify(result)
 
 
 #get x id #######################
@@ -93,6 +97,10 @@ def get_categoria_x_id(id):
     una_categoria = Categoria.query.get(id)
     return categoria_schema.jsonify(una_categoria)
 
+@app.route('/empleado/<id>', methods=['GET'])
+def get_empleado_x_id(id):
+    un_empleado = Empleados.query.get(id)
+    return empleado_schema.jsonify(un_empleado)
 
 #POST ##################
 @app.route('/categorias', methods=['POST'])
@@ -105,6 +113,27 @@ def insert_categoria():
     db.session.add(nuevocategoria)
     db.session.commit()
     return categoria_schema.jsonify(nuevocategoria)
+
+@app.route('/empleado', methods=['POST'])
+def insert_empleado():
+    data = request.get_json(force=True)
+    nombre = data['nombre']
+    domicilio = data['domicilio']
+    telefono = data['telefono']
+    rfc = data['rfc']
+    curp = data['curp']
+    imss = data['imss']
+    id_puesto = data['id_puesto']
+    id_jornada = data['id_jornada']
+    edoEmpleado = data['edoEmpleado']
+    fechaingreso = data['fechaingreso']
+    ctaBancaria = data['ctaBancaria']
+    nuevoempleado = Empleados(nombre, domicilio, telefono, rfc, curp, imss, id_puesto, id_jornada,
+                              edoEmpleado, fechaingreso, ctaBancaria)
+    db.session.add(nuevoempleado)
+    db.session.commit()
+    return empleado_schema.jsonify(nuevoempleado)
+
 
 
 #PUT ########################
@@ -122,6 +151,40 @@ def update_categoria(id):
 
     return categoria_schema.jsonify(actualizarcategoria)
 
+@app.route('/empleado/<id>', methods=['PUT'])
+def update_empleado(id):
+    actualizarempleado= Empleados.query.get(id)
+    data = request.get_json(force=True)
+    nombre = data['nombre']
+    domicilio = data['domicilio']
+    telefono = data['telefono']
+    rfc = data['rfc']
+    curp = data['curp']
+    imss = data['imss']
+    id_puesto = data['id_puesto']
+    id_jornada = data['id_jornada']
+    edoEmpleado = data['edoEmpleado']
+    fechaingreso = data['fechaingreso']
+    ctaBancaria = data['ctaBancaria']
+    actualizarempleado.nombre =nombre
+    actualizarempleado.domicilio = domicilio
+    actualizarempleado.telefono = telefono
+    actualizarempleado.rfc = rfc
+    actualizarempleado.curp = curp
+    actualizarempleado.imss = imss
+    actualizarempleado.id_puesto = id_puesto
+    actualizarempleado.id_jornada = id_jornada
+    actualizarempleado.edoEmpleado = edoEmpleado
+    actualizarempleado.fechaingreso = fechaingreso
+    actualizarempleado.ctaBancaria = ctaBancaria
+
+    db.session.commit()
+    return empleado_schema.jsonify(actualizarempleado)
+
+
+
+
+
 #delete ####
 @app.route('/categorias/<id>', methods=['DELETE'])
 def delete_categoria(id):
@@ -130,6 +193,12 @@ def delete_categoria(id):
     db.session.commit()
     return categoria_schema.jsonify(eliminarcategoria)
 
+@app.route('/empleado/<id>', methods=['DELETE'])
+def delete_empleado(id):
+    eliminarempleado = Empleados.query.get(id)
+    db.session.delete(eliminarempleado)
+    db.session.commit()
+    return empleado_schema.jsonify(eliminarempleado)
 
 
 
@@ -139,4 +208,5 @@ def index():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    db.init_app(app)
+    app.run(debug=True, host='0.0.0.0', port=5000)
